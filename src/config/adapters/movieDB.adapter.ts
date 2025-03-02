4256de38a3bda51fdcaa950a694787d38d020d4a
Victor Mazo <viamazoba@unal.edu.co>
2025-03-01 21:27:04 -0500

    feat: Se quita key para subir código al repo

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -4,7 +4,7 @@ import { AxiosAdapter } from './http/axios.adapter';
 export const movieDBFetcher = new AxiosAdapter({
     baseUrl: 'https://api.themoviedb.org/3/movie',
     params: {
-        api_key: 'cd860cd2cfd32023cd3c6e338bdfc313',
+        api_key: 'Colocar APIKEY',
         language: 'es',
     },
 });
