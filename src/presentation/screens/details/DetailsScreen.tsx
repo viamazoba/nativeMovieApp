cc428fd87a9671baba579e508e4f3705a6affa2c
Victor Mazo <viamazoba@unal.edu.co>
2025-03-04 19:53:43 -0500

    feat: Se maqueta e implementa el componente MovieHeader

--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -2,6 +2,7 @@ import { StackScreenProps } from '@react-navigation/stack';
 import { Text, View } from 'react-native';
 import { RootStackParams } from '../../routes/Navigation';
 import { useMovie } from '../../hooks/useMovie';
+import { MovieHeader } from '../../components/movie/MovieHeader';
 
 
 interface Props extends StackScreenProps<RootStackParams, 'Details'> { }
@@ -9,9 +10,18 @@ interface Props extends StackScreenProps<RootStackParams, 'Details'> { }
 export const DetailsScreen = ({ route }: Props) => {
 
     const { movieId } = route.params;
-    const { movie } = useMovie(movieId);
+    const { isLoading, movie } = useMovie(movieId);
+
+    if (isLoading) {
+        return <Text>Loading</Text>;
+    }
     return (
         <View>
+            <MovieHeader
+                poster={movie.poster}
+                title={movie.title}
+                originalTitle={movie.originalTitle}
+            />
             <Text>Movie: {movie.originalTitle}</Text>
         </View>
     );
