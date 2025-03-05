70b01cea79c66db3f61cb011ae4b85cf51e7403a
Victor Mazo <viamazoba@unal.edu.co>
2025-03-05 07:12:41 -0500

    feat: Se crea use-case, mapper y tipados necesarios para obtener los actores de una película

--- src/core/use-cases/index.ts
+++ src/core/use-cases/index.ts
@@ -5,3 +5,4 @@ export * from './movies/upcoming.use-case';
 export * from './movies/top-rated.use-case';
 export * from './movies/popular.use-case';
 export * from './movie/get-by-id.use-case';
+export * from './movie/get-cast.use-case';
--- src/presentation/hooks/useMovie.tsx
+++ src/presentation/hooks/useMovie.tsx
@@ -3,11 +3,13 @@ import { useEffect, useState } from 'react';
 import * as useCases from '../../core/use-cases/index';
 import { movieDBFetcher } from '../../config/adapters/movieDB.adapter';
 import type { FullMovie } from '../../core/entities/movie.entity';
+import { Cast } from '../../core/entities/cast.entity';
 
 export const useMovie = (movieId: number) => {
 
     const [isLoading, setIsLoading] = useState(true);
     const [movie, setMovie] = useState<FullMovie>({} as FullMovie);
+    const [cast, setCast] = useState<Cast[]>();
 
     useEffect(() => {
         loadMovie();
@@ -15,8 +17,13 @@ export const useMovie = (movieId: number) => {
 
     const loadMovie = async () => {
         setIsLoading(true);
-        const fullMovie = await useCases.getMovieByIdUseCase(movieDBFetcher, movieId);
+        const fullMoviePromise = useCases.getMovieByIdUseCase(movieDBFetcher, movieId);
+        const castPromise = useCases.getMovieCastUseCase(movieDBFetcher, movieId);
+
+        const [fullMovie, castResponse] = await Promise.all([fullMoviePromise, castPromise]);
+
         setMovie(fullMovie);
+        setCast(castResponse);
         setIsLoading(false);
 
     };
@@ -24,5 +31,6 @@ export const useMovie = (movieId: number) => {
     return {
         isLoading,
         movie,
+        cast,
     };
 };
