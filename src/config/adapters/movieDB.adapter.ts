2281190f782a299bda92ad1db4da049e8c1b01cc
Victor Mazo <viamazoba@unal.edu.co>
2025-03-08 11:44:07 -0500

    feat: Se configuran variables de entorno para el proyecto

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -1,10 +1,11 @@
+import { THE_MOVIE_DB_KEY } from '@env';
 import { AxiosAdapter } from './http/axios.adapter';
 
 
 export const movieDBFetcher = new AxiosAdapter({
     baseUrl: 'https://api.themoviedb.org/3/movie',
     params: {
-        api_key: 'Colocar APIKEY',
+        api_key: THE_MOVIE_DB_KEY ?? 'no-key',
         language: 'es',
     },
 });
